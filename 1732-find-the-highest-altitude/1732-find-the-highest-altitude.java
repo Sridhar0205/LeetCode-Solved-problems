6df012214bfbc510d4b248939db69731333a5b50
Sridhar V <007sridharv@gmail.com>
2025-12-30 16:04:52 +0530

    Time: 0 ms (100%), Space: 43.4 MB (6.89%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -3,7 +3,7 @@ class Solution {
       int sum =0;
       int max = 0;
       for(int i =0; i<gain.length;i++){
-        sum = gain[i]+sum;
+        sum += gain[i];
         max = Math.max(max,sum);
       }
       return max;  
